6530611a866366d70bf8b3713d41efb61712a6db
Gorav <goravjindal86@gmail.com>
2025-03-08 12:48:12 +0530

    Medical Diagnosis

---
+++ app.py
@@ -0,0 +1,149 @@
+import streamlit as st
+import pickle
+from streamlit_option_menu import option_menu
+
+st.set_page_config(page_title="Disease Prediction", page_icon="⚕️")
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            body {color: red;}
+            button {color: red; border: 2px solid red; background-color: transparent;}
+            .stTextInput input, .stNumberInput input, .stSelectbox select {
+                color: red;
+                border: 1px solid red;
+            }
+            .stTextInput label {
+        color: red;
+        font-size: 18px;
+    }.stNumberInput label {
+    color: red;
+}
+.stSelectbox label {
+    color: red;
+}
+
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+background_image_url = "https://news.mit.edu/sites/default/files/styles/news_article__image_gallery/public/images/202001/MIT-Diagnostic-Performance_0.jpg?itok=3PhpCCep"  
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] {{
+background-image: url({background_image_url});
+background-size: cover;
+color: red;
+background-position: center;
+background-repeat: no-repeat;
+background-attachment: fixed;
+}}
+
+[data-testid="stAppViewContainer"]::before {{
+content: "";
+position: absolute;
+top: 0;
+left: 0;
+width: 100%;
+height: 100%;
+background-color: rgba(0, 0, 0, 0.7);
+}}
+
+[data-testid="stTextInput > label"] {{
+    color: red;
+}}
+[data-testid="stNumberInput > label"] {{
+    color: red;
+}}
+[data-testid="stSelectbox > label"] {{
+    color: red;
+}}
+[data-testid="stHeader"] {{
+    color: red;
+}}
+[data-testid="stMarkdown"] {{
+    color: red;
+}}
+[data-testid="stSuccess"] {{
+    color: red;
+}}
+[data-testid="stTitle"] {{
+    color: red;
+}}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# Load the saved models
+models = {
+    'diabetes': pickle.load(open('Models/diabetes_model.sav', 'rb')),
+    'heart_disease': pickle.load(open('Models/heart_disease_model.sav', 'rb')),
+    'parkinsons': pickle.load(open('Models/parkinsons_model.sav', 'rb')),
+    'lung_cancer': pickle.load(open('Models/lungs_disease_model.sav', 'rb')),
+    'thyroid': pickle.load(open('Models/Thyroid_model.sav', 'rb'))
+}
+
+# Create a dropdown menu for disease prediction
+selected = st.selectbox(
+    'Select a Disease to Predict',
+    ['Diabetes Prediction',
+     'Heart Disease Prediction',
+     'Parkinsons Prediction',
+     'Lung Cancer Prediction',
+     'Hypo-Thyroid Prediction']
+)
+
+def display_input(label, tooltip, key, type="text"):
+    if type == "text":
+        return st.text_input(label, key=key, help=tooltip)
+    elif type == "number":
+        return st.number_input(label, key=key, help=tooltip, step=1)
+
+# Diabetes Prediction Page
+if selected == 'Diabetes Prediction':
+    st.title('Diabetes')
+    st.write("Enter the following details to predict diabetes:")
+
+    Pregnancies = display_input('Number of Pregnancies', 'Enter number of times pregnant', 'Pregnancies', 'number')
+    Glucose = display_input('Glucose Level', 'Enter glucose level', 'Glucose', 'number')
+    BloodPressure = display_input('Blood Pressure value', 'Enter blood pressure value', 'BloodPressure', 'number')
+    SkinThickness = display_input('Skin Thickness value', 'Enter skin thickness value', 'SkinThickness', 'number')
+    Insulin = display_input('Insulin Level', 'Enter insulin level', 'Insulin', 'number')
+    BMI = display_input('BMI value', 'Enter Body Mass Index value', 'BMI', 'number')
+    DiabetesPedigreeFunction = display_input('Diabetes Pedigree Function value', 'Enter diabetes pedigree function value', 'DiabetesPedigreeFunction', 'number')
+    Age = display_input('Age of the Person', 'Enter age of the person', 'Age', 'number')
+
+    diab_diagnosis = ''
+    if st.button('Diabetes Test Result'):
+        diab_prediction = models['diabetes'].predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+        diab_diagnosis = 'The person is diabetic' if diab_prediction[0] == 1 else 'The person is not diabetic'
+        st.success(diab_diagnosis)
+
+# Heart Disease Prediction Page
+if selected == 'Heart Disease Prediction':
+    st.title('Heart Disease')
+    st.write("Enter the following details to predict heart disease:")
+
+    age = display_input('Age', 'Enter age of the person', 'age', 'number')
+    sex = display_input('Sex (1 = male; 0 = female)', 'Enter sex of the person', 'sex', 'number')
+    cp = display_input('Chest Pain types (0, 1, 2, 3)', 'Enter chest pain type', 'cp', 'number')
+    trestbps = display_input('Resting Blood Pressure', 'Enter resting blood pressure', 'trestbps', 'number')
+    chol = display_input('Serum Cholesterol in mg/dl', 'Enter serum cholesterol', 'chol', 'number')
+    fbs = display_input('Fasting Blood Sugar > 120 mg/dl (1 = true; 0 = false)', 'Enter fasting blood sugar', 'fbs', 'number')
+    restecg = display_input('Resting Electrocardiographic results (0, 1, 2)', 'Enter resting ECG results', 'restecg', 'number')
+    thalach = display_input('Maximum Heart Rate achieved', 'Enter maximum heart rate', 'thalach', 'number')
+    exang = display_input('Exercise Induced Angina (1 = yes; 0 = no)', 'Enter exercise induced angina', 'exang', 'number')
+    oldpeak = display_input('ST depression induced by exercise', 'Enter ST depression value', 'oldpeak', 'number')
+    slope = display_input('Slope of the peak exercise ST segment (0, 1, 2)', 'Enter slope value', 'slope', 'number')
+    ca = display_input('Major vessels colored by fluoroscopy (0-3)', 'Enter number of major vessels', 'ca', 'number')
+    thal = display_input('Thal (0 = normal; 1 = fixed defect; 2 = reversible defect)', 'Enter thal value', 'thal', 'number')
+
+    heart_diagnosis = ''
+    if st.button('Heart Disease Test Result'):
+        heart_prediction = models['heart_disease'].predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
+        heart_diagnosis = 'The person has heart disease' if heart_prediction[0] == 1 else 'The person does not have heart disease'
+        st.success(heart_diagnosis)
+
